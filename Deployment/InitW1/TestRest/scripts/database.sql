90dda01d3c05ae8da774d871e9480e85fb87838a
682556 <682556@01HW742573.India.TCS.com>
2017-03-31 15:50:06 +0530

    Commit 31/03/2017 15:50:06

---
+++ Deployment/InitW1/TestRest/scripts/database.sql
@@ -0,0 +1,21 @@
+--Start Writing Table ddls
+DROP TABLE TESTCOMPONENTNEWCLASS;
+
+CREATE TABLE TESTCOMPONENTNEWCLASS
+(
+	CUSTID NUMERIC(9,0)  NOT NULL  ,
+	CUSTNAME CHARACTER VARYING(256)  ,
+	CUSTCHAR CHARACTER VARYING(1)  ,
+	CUSTDATE DATE ,
+	TestComponentNewClassVer NUMERIC (4, 0)  NOT NULL ,
+	dmLastUpdateDate	TIMESTAMP WITH TIME ZONE	NOT NULL  ,
+	PRIMARY KEY ( 
+	CUSTID  )
+);
+--End Writing Table ddls
+--Start Writing Index ddls
+--End Writing Index ddls
+--Start Writing Keys ddls
+--End Writing Keys ddls
+--Start Writing Sequence ddls
+--End Writing Sequence ddls
